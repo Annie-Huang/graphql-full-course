16580271644449dd9252100030e58a1836988661
Annie Huang <annie.hn@gmail.com>
2024-07-16 21:03:57 +1000

    Add <ApolloProvider> into <App>

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from '@apollo/client';
 
 function App() {
-  return <div className='App'>Hello world</div>;
+  const client = new ApolloClient({
+    uri: 'http://localhost:1338/graphql',
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <div className='App'>Hello world</div>
+    </ApolloProvider>
+  );
 }
 
 export default App;
