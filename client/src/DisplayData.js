7f8db18e059e2e5b284258a8cfa02576e92210bb
Annie Huang <annie.hn@gmail.com>
2024-07-16 21:21:32 +1000

    Add another field in graphql and make sure it worked.

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -8,6 +8,7 @@ const QUERY_ALL_USERS = gql`
       name
       age
       username
+      nationality
     }
   }
 `;
@@ -28,12 +29,13 @@ const DisplayData = () => {
   return (
     <div>
       {data &&
-        data.users.map((user) => {
+        data.users.map((user, index) => {
           return (
-            <div>
+            <div key={index}>
               <h1>Name: {user.name}</h1>
               <h1>UserName: {user.username}</h1>
               <h1>Age: {user.age}</h1>
+              <h1>Nationality: {user.nationality}</h1>
             </div>
           );
         })}
